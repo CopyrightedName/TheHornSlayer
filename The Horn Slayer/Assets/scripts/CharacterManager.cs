238f2d38ae2ae2ab39adb945fb65bb228b953319
Daniel Hritz <danielhritz123@gmail.com>
2018-05-30 23:37:59 +0200

    shooting

--- The Horn Slayer/Assets/scripts/CharacterManager.cs
+++ The Horn Slayer/Assets/scripts/CharacterManager.cs
@@ -7,20 +7,43 @@ public class CharacterManager : MonoBehaviour {
     public Animator hornAnim;
     public AudioSource hornSound;
 
+    GameObject bulletInstance;
+    public GameObject bullet;
+    public Transform pos;
+
+    bool canFire;
+    public float maxTime;
+    float time;
+    public float bulletForce;
+
 	void Start () {
-		
 	}
 	
 	void Update () {
-        if (Input.GetMouseButtonDown(0))
+
+
+        if (Input.GetMouseButtonDown(0) && canFire)
         {
             hornAnim.SetBool("canFire", true);
             hornSound.Play();
+            canFire = false;
+            bulletInstance = Instantiate(bullet, pos.position, Quaternion.identity);
+            bulletInstance.GetComponent<Rigidbody>().AddForce(pos.transform.forward * bulletForce);
         }
 
         if (Input.GetMouseButtonUp(0))
         {
             hornAnim.SetBool("canFire", false);
+        }
+
+        if(canFire == false)
+        {
+            time--;
+        }
+        if(time <= 0)
+        {
+            canFire = true;
+            time = maxTime;
         }
     }
 }
