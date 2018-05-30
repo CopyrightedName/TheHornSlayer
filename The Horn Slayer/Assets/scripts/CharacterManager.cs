c523ba1e542afed16a762dbe95485f1c11839392
Daniel Hritz <danielhritz123@gmail.com>
2018-05-30 23:13:33 +0200

    update

---
+++ The Horn Slayer/Assets/scripts/CharacterManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterManager : MonoBehaviour {
+
+    public Animator hornAnim;
+    public AudioSource hornSound;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            hornAnim.SetBool("canFire", true);
+            hornSound.Play();
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            hornAnim.SetBool("canFire", false);
+        }
+    }
+}
